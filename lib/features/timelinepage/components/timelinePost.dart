997cce79aa860df4ee62bcbba362dad4ebc0089b
mhikmal03 <hikmalkp@gmail.com>
2023-02-25 20:08:49 +0700

    timeline blur done

--- lib/features/timelinepage/components/timelinePost.dart
+++ lib/features/timelinepage/components/timelinePost.dart
@@ -1,7 +1,19 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class timelinePost extends StatefulWidget {
-  const timelinePost({super.key});
+  final String creator;
+  final String description;
+  final String image;
+  final bool blur;
+  const timelinePost({
+    required this.creator,
+    required this.description,
+    required this.image,
+    required this.blur,
+  });
 
   @override
   State<timelinePost> createState() => _timelinePostState();
@@ -10,64 +22,97 @@ class timelinePost extends StatefulWidget {
 class _timelinePostState extends State<timelinePost> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Container(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 18,
-                  backgroundImage: AssetImage('assets/jerometimeline.jpg'),
-                  backgroundColor: Colors.grey[300],
-                  // add a white border with thickness 2 around the circle
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Jerome Polin",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Container(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundImage: AssetImage(widget.image),
+                backgroundColor: Colors.grey[300],
+                // add a white border with thickness 2 around the circle
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.creator,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    widget.description,
+                    style: TextStyle(
+                      fontSize: 16,
                     ),
-                    const SizedBox(
-                      height: 5,
+                  )
+                ],
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                '10 menit yang lalu',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              ),
+            ],
+          ),
+        ),
+      ),
+      ClipRRect(
+        borderRadius: BorderRadius.circular(
+            20), // sets the border radius of the rectangle
+        child: widget.blur
+            ? Stack(children: [
+                Image.asset(
+                  widget.image,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+                Positioned.fill(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 10,
+                      sigmaY: 10,
                     ),
-                    Text(
-                      'Lagi di Jepang!',
+                    child: Container(
+                      color: Colors.black.withOpacity(0),
+                    ),
+                  ),
+                ),
+                Positioned.fill(
+                  child: Center(
+                    child: Text(
+                      
+                      'Upgrade ke Premium untuk melihat dan beri reaksi!',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 16,
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        
                       ),
-                    )
-                  ],
+                      
+                    ),
+                  ),
                 ),
-                const SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  '10 menit yang lalu',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                ),
-              ],
-            ),
-          ),
-        ),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(
-              20), // sets the border radius of the rectangle
-          child: Image.asset(
-            'assets/jerometimeline.jpg',
-            width: double.infinity,
-            height: 300,
-            fit: BoxFit.cover,
-          ),
-        ),
-      ],
-    );
+              ])
+            : Image.asset(
+                widget.image,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+      ),
+    ]);
   }
 }
--- lib/features/timelinepage/timelinePage.dart
+++ lib/features/timelinepage/timelinePage.dart
@@ -9,18 +9,43 @@ class timelinePage extends StatefulWidget {
 }
 
 class _timelinePageState extends State<timelinePage> {
+
+  List<Map<String, dynamic>> posts = [
+    {
+      'creator': 'Jerome Polin',
+      'description': 'lagi di Jepang',
+      'image': 'assets/jerometimeline.jpg',
+      'blur': false,
+    },
+    {
+      'creator': 'Urrofi',
+      'description': 'Ngonten terus',
+      'image': 'assets/urrofi.png',
+      'blur': false,
+    },
+    {
+      'creator': 'Kirana Pratiwi',
+      'description': 'kuliah gais!',
+      'image': 'assets/kiranaputri.png',
+      'blur': true,
+    },
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.only(bottom: 20),
         child: Column(
-          children: [
-            timelinePost(),
-            timelinePost(),
-            timelinePost(),
-            timelinePost(),
-          ],
+          children: posts.map((item) {
+            return timelinePost(
+              creator: item['creator'],
+              description: item['description'],
+              image: item['image'],
+              blur: item['blur']
+            );
+          }).toList(),
         ),
       )
     );
